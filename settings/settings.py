0756253a2e91e161ce3f6299833585447b14146a
Nikolay Sokolov <cto@mineinvest.ru>
2021-06-11 17:31:47 +0300

    lesson 10 june 2021

--- settings/settings.py
+++ settings/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y#-6#em1-v^o92c&j+^h1*x^)2iztth%b#@%rt90ct4q7)e=zu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
